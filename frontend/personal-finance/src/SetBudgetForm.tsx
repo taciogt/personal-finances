53d72af03e1bb24586c97a0f79b09b1341cfacd2
Tácio <taciogt@gmail.com>
2021-06-27 18:25:25 -0300

    #1 sends amount from the frontend to the backend

--- frontend/personal-finance/src/SetBudgetForm.tsx
+++ frontend/personal-finance/src/SetBudgetForm.tsx
@@ -1,4 +1,4 @@
-import {FC, FormEvent} from "react";
+import {ChangeEvent, FC, FormEvent, useState} from "react";
 import {FormControl, FormHelperText, Input, InputLabel} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import axios from "axios";
@@ -8,19 +8,34 @@ const apiClient = axios.create({
   headers: {'Content-Type' : 'application/json'}
 });
 
+class Budget {
+  constructor(public readonly amount?: number) {}
+}
+
 export const SetBudgetForm: FC = () => {
+  const [budget, setBudget] = useState(new Budget())
+
+  function changeBudgetAmountHandler(event: ChangeEvent<HTMLInputElement>){
+    setBudget(prevBudget => {
+      const newAmount = parseInt(event.target.value)
+      return {
+        ...prevBudget,
+        amount: newAmount
+      }
+    })
+  }
+
   function submitHandler(event: FormEvent) {
     event.preventDefault()
-    console.log(event)
 
     const data = {
       budget: {
-        amount: 1000,
         essentials: 0.55,
         "education": 0.05,
         "goals": 0.2,
         "retirement": 0.1,
-        "loose": 0.1
+        "loose": 0.1,
+        ...budget
       }
     }
 
@@ -37,7 +52,8 @@ export const SetBudgetForm: FC = () => {
       <form onSubmit={submitHandler}>
         <FormControl>
           <InputLabel htmlFor="total-amount-input">Valor total</InputLabel>
-          <Input id="total-amount-input" aria-describedby="total-amount-helper-text"/>
+          <Input id="total-amount-input" aria-describedby="total-amount-helper-text"
+          value={budget.amount} onChange={changeBudgetAmountHandler} />
           <FormHelperText id="total-amount-helper-text">Valor base para o orçamento</FormHelperText>
         </FormControl>
         <Button variant="contained" color="primary" type="submit">
