58301f28952647e864ca3fe8a46d1c507b1157df
Tácio <taciogt@gmail.com>
2021-11-05 23:19:42 -0300

    #11 fixes more eslint errors

--- frontend/personal-finance/src/Menu.tsx
+++ frontend/personal-finance/src/Menu.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import MenuIcon from '@material-ui/icons/Menu'
+import React from 'react'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -19,10 +19,10 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
     },
   }),
-);
+)
 
 export default function Menu() {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <div className={classes.root}>
@@ -38,5 +38,5 @@ export default function Menu() {
         </Toolbar>
       </AppBar>
     </div>
-  );
+  )
 }
--- frontend/personal-finance/src/components/BowlSetter.tsx
+++ frontend/personal-finance/src/components/BowlSetter.tsx
@@ -1,8 +1,8 @@
-import {FC} from "react";
-import {Grid} from "@material-ui/core";
-import Typography from "@material-ui/core/Typography";
-import {PercentageSlider} from "./PercentageSlider";
-import {Budget, BudgetBowls, centsToBRL, getBudgetBowlAmount} from "../domain/Budget";
+import { Grid } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
+import React, { FC } from 'react'
+import { Budget, BudgetBowls, centsToBRL, getBudgetBowlAmount } from '../domain/Budget'
+import { PercentageSlider } from './PercentageSlider'
 
 interface bowlSetterProps {
     title: string
@@ -12,16 +12,16 @@ interface bowlSetterProps {
 }
 
 export const BowlSetter: FC<bowlSetterProps> = ({title, bowlName, budget, valueChangeHandler}) => {
-    return <>
-        <Grid container spacing={2} alignItems="flex-end" justify="space-around">
-            <Grid item xs={8}>
-                <PercentageSlider title={title} value={budget[bowlName]}
-                                  valueChangeHandler={valueChangeHandler}/>
-            </Grid>
-            <Grid item xs={4}>
-                <Typography variant={'body1'}>{centsToBRL(getBudgetBowlAmount(budget, bowlName))}</Typography>
-            </Grid>
-        </Grid>
-    </>
+  return <>
+    <Grid container spacing={2} alignItems="flex-end" justify="space-around">
+      <Grid item xs={8}>
+        <PercentageSlider title={title} value={budget[bowlName]}
+          valueChangeHandler={valueChangeHandler}/>
+      </Grid>
+      <Grid item xs={4}>
+        <Typography variant={'body1'}>{centsToBRL(getBudgetBowlAmount(budget, bowlName))}</Typography>
+      </Grid>
+    </Grid>
+  </>
 
 }
