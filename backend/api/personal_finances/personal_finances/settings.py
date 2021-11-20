3926ebc9982dde5e4871123343d339dac433f623
Tácio <taciogt@gmail.com>
2021-11-20 17:43:53 -0300

    test

--- backend/api/personal_finances/personal_finances/settings.py
+++ backend/api/personal_finances/personal_finances/settings.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'personal_finances.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
