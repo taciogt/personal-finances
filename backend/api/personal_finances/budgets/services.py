d7d0c8080371367df533181ee698fc65230c0633
Tácio Tavares <taciogt@gmail.com>
2022-04-21 10:55:00 -0300

    #14 fixes flake8 errors

--- backend/api/personal_finances/budgets/services.py
+++ backend/api/personal_finances/budgets/services.py
@@ -1,10 +1,9 @@
-from budgets.repositories import DjangoBudgetRepository
-from core.budgets.use_cases import set_budget as core_set_budget, get_budget as core_get_budget
-from core.budgets.repositories import BudgetRepositoryInMemory
-# from core.budgets.entities import Budget
 from functools import partial
 
-# repository = BudgetRepositoryInMemory()
+from budgets.repositories import DjangoBudgetRepository
+
+from core.budgets.use_cases import set_budget as core_set_budget, get_budget as core_get_budget
+
 repository = DjangoBudgetRepository()
 
 set_budget = partial(core_set_budget, repository=repository)
