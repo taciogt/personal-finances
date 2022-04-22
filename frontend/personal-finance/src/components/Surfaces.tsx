096ae427afca68b0d543130ecb6f5d92dfcbd30e
Tácio Tavares <taciogt@gmail.com>
2022-04-22 20:30:28 -0300

    #24 creates assets list table

--- frontend/personal-finance/src/components/Surfaces.tsx
+++ frontend/personal-finance/src/components/Surfaces.tsx
@@ -1,9 +1,6 @@
 import {styled} from '@material-ui/core/styles'
-import {Card, Paper} from '@material-ui/core'
+import {Paper} from '@material-ui/core'
 
-export const StyledPageCard = styled(Paper)({
+export const PageContainer = styled(Paper)({
   padding: 10,
-  // maxWidth: '90%',
-  // alignContent: 'center',
-  // margin: ' 0 5% 0 5%'
 })
--- frontend/personal-finance/src/pages/AssetsList.tsx
+++ frontend/personal-finance/src/pages/AssetsList.tsx
@@ -1,13 +1,89 @@
-import React, {FC, ReactElement} from 'react'
-import {Card, Typography} from '@material-ui/core'
-import {StyledPageCard} from '../components/Surfaces'
+import React, {FC} from 'react'
+import {
+  Box,
+  Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography
+} from '@material-ui/core'
+import {PageContainer} from '../components/Surfaces'
+import {makeStyles, styled} from '@material-ui/core/styles'
+import {centsToBRL} from '../domain/Budget'
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+})
+
+class AssetGroup {
+  constructor(
+    public name: string
+  ) {
+  }
+}
+
+class Asset {
+  constructor(
+    public name: string,
+    public currentValue: number,
+    public group: AssetGroup
+  ) {
+  }
+}
+
+const assetGroups = [
+  new AssetGroup('Pós-fixado'),
+  new AssetGroup('Pré-fixado'),
+  new AssetGroup('Inflação'),
+  new AssetGroup('Multimercado'),
+  new AssetGroup('Renda Variável'),
+  new AssetGroup('internacional'),
+
+]
+
+const assets = [
+  new Asset('Poupanca', 10000, assetGroups[0]),
+  new Asset('Tesouro Selic', 250000, assetGroups[0]),
+  new Asset('Fundo Multimercado', 2000, assetGroups[3]),
+  new Asset('Carteira de Ações', 15000, assetGroups[4]),
+]
+
+const StyledBox = styled(Box)(({theme}) => ({
+  padding: theme.spacing(2, 0),
+}))
 
 export const AssetsList: FC = () => {
+  const classes = useStyles()
+
   return (
-    <StyledPageCard elevation={3}>
-      <Card>
-        <Typography>Ativos Financeiros</Typography>
-      </Card>
-    </StyledPageCard>
+    <PageContainer elevation={3}>
+      <Typography variant='h5'>Ativos Financeiros</Typography>
+      <StyledBox><Divider/></StyledBox>
+      <TableContainer>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell>Grupo</TableCell>
+              <TableCell>Valor Atual</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {assets.map(asset =>
+              <TableRow key={asset.name}>
+                <TableCell>{asset.name}</TableCell>
+                <TableCell>{asset.group.name}</TableCell>
+                <TableCell>{centsToBRL(asset.currentValue)}</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </PageContainer>
   )
 }
